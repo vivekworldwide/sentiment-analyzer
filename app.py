308b22d1394f74cbd1572054e9431e282cdbcd96
vivekworldwide <vivek.baghel4149@gmail.com>
2026-03-29 08:32:49 +0530

    written code in app.py and index.html

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import pickle
+import re
+
+app = Flask(__name__)
+
+model = pickle.load(open('model.pkl', 'rb'))
+tfidf = pickle.load(open('tfidf.pkl','rb'))
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r'<.*?>', '',text)
+    text = re.sub(r'[^a-zA-Z ]', '',text)
+    text = text.lower()
+    return text
+
+@app.route('/', methods= ['GET','POST'])
+def home():
+    result = None
+    if request.method == 'POST':
+        review = request.form['review']
+        cleaned_review = clean_text(review)
+        vector = tfidf.transform([cleaned])
+        prediction = model.predict(vector)[0]
+        result = prediction
+    return render_template('index.html', result = result)
+
+if __name__ == '__main__':
+    app.run(debug = True)
